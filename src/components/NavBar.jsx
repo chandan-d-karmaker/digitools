9f5184e407c84e200d18a2319520ac2b4508e8b5
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-03-29 22:47:50 +0600

    made responsive banner section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
-                <div className="navbar-start">
+                <div className="navbar-start flex items-center gap-2">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -21,7 +21,9 @@ const NavBar = () => {
                             <li><a className='font-semibold'>FAQ</a></li>
                         </ul>
                     </div>
-                    <img src={Logo} alt="Logo" />
+                    <div>
+                        <img src={Logo} alt="Logo" />
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
