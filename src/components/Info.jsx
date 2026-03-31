fd4cd9f8ac615fc820a3a4ee630f066d560d6880
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-03-31 12:53:30 +0600

    fixed mobile stats

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Info = () => {
     return (
         <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-14'>
-            <div className='flex flex-wrap justify-between items-center w-4/5 mx-auto text-white text-center'>
+            <div className='flex flex-col justify-between items-center w-4/5 mx-auto text-white text-center md:flex-row '>
                 <div className='mb-5 md:mb-0'>
                     <h2 className='font-bold text-6xl'>50K+</h2>
                     <p className='text-2xl font-medium opacity-80'>Active Users</p>
