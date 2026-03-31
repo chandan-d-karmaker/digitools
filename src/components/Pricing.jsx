85b4b35966d1ce9d336c38e01fcf777428aa1a04
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-03-31 13:05:14 +0600

    fixed mobile price section font size

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -9,7 +9,7 @@ const Pricing = ({ pricesPromise }) => {
         <div className='py-28 w-4/5 mx-auto'>
 
             <div className='text-center mb-10'>
-                <h1 className='text-5xl font-extrabold mb-4 text-[#101727]'>Simple, Transparent Pricing</h1>
+                <h1 className='md:text-5xl text-4xl font-extrabold mb-4 text-[#101727]'>Simple, Transparent Pricing</h1>
                 <p className='text-[16px] text-[#627382]'>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
